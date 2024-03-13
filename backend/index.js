b6134427e208db153b3c616421412d20d3a035ec
Harshit <harshit.mathur.official@gmail.com>
2024-03-13 15:38:29 +0530

    index css update

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+require ('./app/config/mongodb')
 
 const app = express();
 
 app.get('',(request,response) => {
     response.send('Server is working fine.');
 });
+
+
 
 app.listen(5000, ()=> 
     console.log("Backend is running")
